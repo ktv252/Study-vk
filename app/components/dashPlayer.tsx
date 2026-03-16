409727a63d92e92973d86ed921d693164427a863
TejaswiniDKakade <tejukakade.01@gmail.com>
2026-03-16 20:35:29 +0530

    fix: force live streams to join from start of recording

--- app/components/dashPlayer.tsx
+++ app/components/dashPlayer.tsx
@@ -300,11 +300,7 @@ const VideoPlayer: React.FC<Props> = ({
             },
             clearKeys: drmConfig.clearKeys,
           },
-          streaming: {
-            startAtLiveEdge: false,
-          },
         });
-
       }
 
       if (signedUrlQuery) {
